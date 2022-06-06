674e3a7b02eb9f4c5968ff8a14bd434ac72c4e8c
t-riku <dingariku@icloud.com>
2022-06-06 17:13:45 +0900

    fix header menu

--- js/menu.js
+++ js/menu.js
@@ -17,49 +17,24 @@ upButton.addEventListener("click", function () {
 
 // ハンバーガーメニュー
 const menuToggle = document.querySelector(".toggle");
-const topPage = document.querySelector(".toppage-box");
-const menuZ = document.querySelector(".menu");
+const topPageBox = document.querySelector(".toppage-box");
+const topPage = document.querySelector(".toppage");
+const menu = document.querySelector(".menu");
 
 menuToggle.addEventListener("click", function () {
   // activeにする
   menuToggle.classList.toggle("active");
+  topPageBox.classList.toggle("active");
   topPage.classList.toggle("active");
-  menuZ.classList.toggle("active");
+  menu.classList.toggle("active");
+  //menu.classList.toggle("active");
 });
 
-// topPage.addEventListener("click", function () {
-//   // if (menuToggle.hasClass("active")) {
-//   //   menuToggle.removeClass("active");
-//   //   topPage.removeClass("active");
-//   // }
-//   if (menuToggle.hasClass("active")) {
-//     menuToggle.classList.toggle("active");
-//     topPage.classList.toggle("active");
-//   }
-// });
-
-// const aa = document.querySelector(".toppage-box.active");
-
-// aa.addEventListener("click", (e) => {
-//   if (!e.target.classList.contains("active")) {
-//     //ここに外側をクリックしたときの処理
-//     menuToggle.classList.remove("active");
-//     topPage.classList.remove("active");
-//   }
-// });
-// topPage.addEventListener("click", (e) => {
-//   if (!e.target.closest(".toggle")) {
-//     //ここに外側をクリックしたときの処理
-//     menuToggle.classList.toggle("active");
-//     topPage.classList.toggle("active");
-//   }
-// });
-
-// jQuery(document).ready(function ($) {
-//   $(".toppage").click(function () {
-//     //ここでnavの部分がactiveクラスを持っているか確認
-//     if (menuToggle.hasClass("active")) {
-//       $(".toggle").removeClass("active");
-//     }
-//   });
-// });
+topPageBox.addEventListener("click", (e) => {
+  if (topPageBox.classList.contains("active")) {
+    //ここに外側をクリックしたときの処理
+    topPageBox.classList.toggle("active");
+    menu.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  }
+});
